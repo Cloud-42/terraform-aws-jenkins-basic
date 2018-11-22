30a92f3b476ab4b3901dbd6388b28c32261e4a05
Chris Morgan <chris.morgan@perspectum-diagnostics.com>
2018-11-22 10:25:10 +0000

    removed non required outputs

--- outputs.tf
+++ outputs.tf
@@ -10,10 +10,3 @@ output "ec2_sg" {
   value = "${aws_security_group.ec2_sg.id}"
 }
 
-output "jenkins_test_user_access_key_id" {
-  value = "${aws_iam_access_key.jenkins_access_key.id}"
-}
-
-output "jenkins_test_user_secret" {
-  value = "${aws_iam_access_key.jenkins_access_key.encrypted_secret}"
-}
